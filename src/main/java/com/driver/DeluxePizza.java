283f1cd05e1558d8c9ad3cb86ce7476a121f7e22
viswanadh800 <viswanadh.800@gmail.com>
2024-01-27 16:56:40 +0530

    Atmost one extracheese and one toppings

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,7 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        isDelux=true;
         addExtraCheese();
         addExtraToppings();
         // your code goes here
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,13 +8,13 @@ public class Pizza {
     private int basePrice;
     private int extraCheesePrice;
     private int extraToppingsPrice;
-    private int finalExtraCheesePrice;
-    private int finalExtraToppingsPrice;
+  //  private int finalExtraCheesePrice;
+   // private int finalExtraToppingsPrice;
+    protected boolean isDelux;
     private int bagPrice;
     private boolean isBag;
     private boolean isCheese;
     private boolean isToppings;
-    protected boolean isDelux;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         extraCheesePrice=80;
@@ -28,11 +28,12 @@ public class Pizza {
             extraToppingsPrice=120;
         }
         price=basePrice;
-        finalExtraCheesePrice=0;
-        finalExtraToppingsPrice=0;
+      //  finalExtraCheesePrice=0;
+       // finalExtraToppingsPrice=0;
         isBag=false;
         isCheese=false;
         isToppings=false;
+        isDelux=false;
     }
 
     public int getPrice(){
@@ -40,15 +41,20 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        finalExtraCheesePrice+=extraCheesePrice;
-        price+=extraToppingsPrice;
-        isCheese=true;
+        if(!isCheese){
+        //    finalExtraCheesePrice+=extraCheesePrice;
+            price+=extraToppingsPrice;
+            isCheese=true;
+        }
     }
 
     public void addExtraToppings(){
-        finalExtraToppingsPrice+=extraToppingsPrice;
-        price+=extraToppingsPrice;
-        isToppings=true;
+      //  System.out.println("Added Extra Toppings");
+        if(!isToppings){
+        //    finalExtraToppingsPrice+=extraToppingsPrice;
+            price+=extraToppingsPrice;
+            isToppings=true;
+        }
     }
 
     public void addTakeaway(){
@@ -61,18 +67,33 @@ public class Pizza {
     public String getBill(){
         StringBuilder sb=new StringBuilder();
 
+        if(isDelux){
+            sb.append("Base Price Of The Pizza: ");
+            sb.append(basePrice+extraToppingsPrice+extraCheesePrice);
+            sb.append('\n');
+            if(isBag){
+                sb.append("Paperbag Added: ");
+                sb.append(bagPrice);
+                sb.append('\n');
+            }
+            sb.append("Total Price: ");
+            sb.append(price);
+            sb.append('\n');
+            return sb.toString();
+        }
+
         sb.append("Base Price Of The Pizza: ");
         sb.append(basePrice);
         sb.append('\n');
 
         if(isCheese){
             sb.append("Extra Cheese Added: ");
-            sb.append(finalExtraCheesePrice);
+            sb.append(extraCheesePrice);
             sb.append('\n');
         }
         if(isToppings){
             sb.append("Extra Toppings Added: ");
-            sb.append(finalExtraToppingsPrice);
+            sb.append(extraToppingsPrice);
             sb.append('\n');
         }
         if(isBag){
